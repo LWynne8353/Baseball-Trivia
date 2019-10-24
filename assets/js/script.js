ee13b4f1a9fa0e8b23b708ec536973f687463e16
liamwynne <liamdwynne5753@gmail.com>
2019-10-24 16:59:59 -0400

    use the hide/show function and added timer to start.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,46 @@
-$(document).ready(function(){
-$("btn start").onclick(function(){
-    $(this).hide();
-});
+//$(document).ready(function() {
+    var number= 100;
+
+    var intervalId;
+
+function run () {
+ clearInterval(intervalId);
+ intervalID = setInterval(decrement, 1000);
+
+}
+
+function decrement(){
+    number --;
+
+$("#Time").html("<h2>" + number + "</h2");
+}
+
+$(".start").click(function(){
+    $("#container").hide();
+    $("#questions").show();
+    console.log( "journey to space")
+
+    run();
+
+
+
+
+
+
+
+
+
+//Next Get Questions and Timer to show and Timer to start.
+
+//$("#questions").hide(function()){
+ //    var container = questions;
+
+//}
+
+
+
    
 
+    });
 
-
-});
+//});
