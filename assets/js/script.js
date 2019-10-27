9718b969d5c9d256a0976621f7334e67a8d45667
liamwynne <liamdwynne5753@gmail.com>
2019-10-26 20:51:15 -0400

    fixed answer clicks so each question can be answered.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,70 @@
-//$(document).ready(function() {
-    var number= 100;
+//$(document).ready(function(){
+// Questions and Timer to show and when start button is clicked.
+var number = 30;
 
-    var intervalId;
+var intervalID;
 
-function run () {
- clearInterval(intervalId);
- intervalID = setInterval(decrement, 1000);
+function run() {
+
+    intervalID = setInterval(decrement, 1000);
+    // clearInterval(intervalId);
 
 }
 
-function decrement(){
-    number --;
 
-$("#Time").html("<h2>" + number + "</h2");
+function decrement() {
+    number--;
+
+    $("#Time").html("<h2>" + number + "</h2");
+
+    if (number == 0) {
+        stop();
+        console.log(number == 0);
+        console.log("stop works");
+    }
 }
 
-$(".start").click(function(){
+
+function stop() {
+    clearInterval(intervalID);
+    console.log("the stop function works");
+}
+
+
+$(".start").click(function () {
     $("#container").hide();
     $("#questions").show();
-    console.log( "journey to space")
+    console.log("journey to space")
 
     run();
 
 
 
 
+});
+
+
+$("#input[type='button']").click(function() {
+    var radioValue= $("input[answer ='correct' ]: checked").val();
+      if (radioVal)
 
 
 
 
 
-//Next Get Questions and Timer to show and Timer to start.
-
-//$("#questions").hide(function()){
- //    var container = questions;
-
-//}
 
 
 
-   
-
+    $(".submit").click(function () {
+        $("#questions").hide();
+        $("#results").show();
+        console.log("results")
     });
 
-//});
+});
+
+
+
+
+
+
